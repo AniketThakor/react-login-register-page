8b2c2e2630892ee5bd54136baaabc1b4e4424073
Aniket Thakor <you@example.com>
2022-04-20 17:35:21 +0530

    Updates

---
+++ precache-manifest.728911c9fa2affb99917a43f1ac426d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1836b821b42a98f3d9d5",
+    "url": "/react-login-register-page/static/css/main.34e09d2d.chunk.css"
+  },
+  {
+    "revision": "1836b821b42a98f3d9d5",
+    "url": "/react-login-register-page/static/js/main.1836b821.chunk.js"
+  },
+  {
+    "revision": "a256ef656d573284e022",
+    "url": "/react-login-register-page/static/js/1.a256ef65.chunk.js"
+  },
+  {
+    "revision": "58902643f7df5d61ba33",
+    "url": "/react-login-register-page/static/js/runtime~main.58902643.js"
+  },
+  {
+    "revision": "81c0550d87d07e87c7bb2f47bd2482c4",
+    "url": "/react-login-register-page/static/media/bg.81c0550d.png"
+  },
+  {
+    "revision": "15b602a3950ac2b409c51a430eb1905f",
+    "url": "/react-login-register-page/index.html"
+  }
+];
